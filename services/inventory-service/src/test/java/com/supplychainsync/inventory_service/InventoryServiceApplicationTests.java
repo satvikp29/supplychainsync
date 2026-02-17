4683ab7c49212c7046e19e03208aa5a092010daf
satvikp29 <satvikp29@gmail.com>
2026-02-17 15:47:01 -0600

    Disable context load test until infra configured

---
+++ services/inventory-service/src/test/java/com/supplychainsync/inventory_service/InventoryServiceApplicationTests.java
@@ -0,0 +1,9 @@
+package com.supplychainsync.inventory_service;
+
+import org.junit.jupiter.api.Disabled;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled("Disabled until infrastructure (DB/Kafka) is configured")
+@SpringBootTest
+class InventoryServiceApplicationTests {
+}
